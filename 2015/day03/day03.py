3b781772d607c3a3c561f74433823160f6ec71c7
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2022-11-29 13:33:56 +0000

    Day 3 part 2 complete

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -18,8 +18,6 @@ def updatecurpos(curpos, move):
         cp2 = (curpos[0]+1, curpos[1])
     else:
         raise ValueError('Invalid move: %c', move)
-    
-
     return(cp2)
 
 def process_a():
@@ -33,17 +31,37 @@ def process_a():
             if not ch:
                 break
             else:
-                print("Move from ", curpos, "with direction", ch, end='')
+                # print("Move from ", curpos, "with direction", ch, end='')
                 moves += 1
                 curpos = updatecurpos(curpos,ch)
-                print(" to", curpos)
+                # print(" to", curpos)
                 visits.add(curpos)
     print("Total moves: ", moves)
     print("Final pos:   ", curpos)
     print("Ttl visits:  ",len(visits))
 
+def process_b():
+    santapos = (0,0)
+    robo_pos = (0,0)
+    visits = set()
+    with open('input.txt') as infile:
+        visits.add(santapos)
+        # no need to add initial robopos as it's the same
+        while True:
+            sp = infile.read(1) # get Santa instruction
+            rp = infile.read(1) # get robot instruction
+            if not sp:
+                break
+            else:
+                santapos = updatecurpos(santapos, sp)
+                robo_pos = updatecurpos(robo_pos, rp)
+                visits.add(santapos)
+                visits.add(robo_pos)
+    print("Houses with at least one present: ", len(visits))
+
 def process():
     process_a()
+    process_b()
    
 if __name__ == "__main__":
     process()
