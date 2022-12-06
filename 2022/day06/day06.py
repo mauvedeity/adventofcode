ad4f27f10a97df5f643f14aadc0a10daa8ace5bd
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2022-12-06 13:59:57 +0000

    Day 6 done

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -1,13 +1,34 @@
 #!env python3 
+#
+# Day 06
+#
+
+def ismarker(pmrk):
+    return(len(set(pmrk)) == len(pmrk))
+
+def scan_a(pli,pmrksz):
+    for idx in range((pmrksz), len(pli)):
+        if ismarker(pli[idx-pmrksz:idx]):
+            return idx
 
 def process_a():
+    with open('inputx.txt') as infile:
+        for li in infile:
+            li = li.strip()
+            markerpos = scan_a(li,4)
+            print("Marker at: ", markerpos)
     return
 
 def process_b():
+    with open('input.txt') as infile:
+        for li in infile:
+            li = li.strip()
+            markerpos = scan_a(li,14)
+            print("Marker at: ", markerpos)
     return
 
 def process():
-    process_a()
+    # process_a()
     process_b()
 
 if __name__ == "__main__":
