7993c302bb62bb94372cf55b038e723ddfe8277e
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2024-12-02 12:42:37 +0000

    Day 2 completed

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -1,14 +1,48 @@
 #!env python3 
 
-def process_a():
-    return
+def checkLine(idat):
+    if(idat[0] > idat[1]):
+        chkvals = [1,2,3]
+    else:
+        chkvals = [-1,-2,-3]
+    thisRepSafe = True
+    for i in range(0, len(idat)-1):
+        x = idat[i]
+        y = idat[i+1]
+        thisRepSafe = thisRepSafe and ((x - y) in chkvals)
+    return(thisRepSafe)
 
-def process_b():
-    return
+def process_a(pstrfname = 'input.txt'):
+    with open(pstrfname, 'rt') as input:
+        safeReps = 0
+        for iline in input:
+            ida1 = iline.strip().split(' ')
+            idat = list(map(int, ida1))
+            if checkLine(idat):
+                safeReps += 1
+    return(safeReps)
+
+def process_b(pstrfname = 'input.txt'):
+    with open(pstrfname, 'rt') as input:
+        safeReps = 0
+        for iline in input:
+            ida1 = iline.strip().split(' ')
+            idat = list(map(int, ida1))
+            if checkLine(idat):
+                safeReps += 1
+            else:
+                thisRepSafe = False
+                for i in range(0, len(idat)):
+                    tlist = idat.copy()
+                    tlist.pop(i)
+                    thisRepSafe = thisRepSafe or checkLine(tlist)
+                if thisRepSafe:
+                    safeReps += 1
+    return(safeReps)
 
 def process():
-    process_a()
-    process_b()
+    print(process_a('input.txt'))
+    print(process_b('input.txt'))
 
 if __name__ == "__main__":
     process()
