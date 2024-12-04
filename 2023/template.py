d9b62884fc7fe411e24e4e80e53ff419aa64736c
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2024-12-04 16:18:46 +0000

    Day 2 Part 1 done

--- 2023/template.py
+++ 2023/template.py
@@ -1,14 +1,14 @@
 #!env python3 
 
-def process_a():
+def process_a(pstrfname = 'input.txt'):
     return
 
-def process_b():
+def process_b(pstrfname = 'input.txt'):
     return
 
 def process():
-    process_a()
-    process_b()
+    print(process_a('input.txt'))
+    print(process_b('input.txt'))
 
 if __name__ == "__main__":
     process()
--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -1,14 +1,30 @@
 #!env python3 
 
-def process_a():
-    return
+import re
 
-def process_b():
+def do_mul(pinstr):
+    # 'mul(ddd,ddd)'
+    lop,rop = pinstr.split(',')
+    lop = lop[4:]
+    rop = rop[:-1]
+    return(int(lop) * int(rop))
+
+def process_a(pstrfname = 'input.txt'):
+    SEARCH = r'mul\(\d+,\d+\)'
+    result = 0
+    with open(pstrfname, 'rt') as infile:
+        for i in infile:
+            m1 = re.findall(SEARCH, i)
+            m2 = sum(list(map(do_mul, m1)))
+            result += m2
+    return(result)
+
+def process_b(pstrfname = 'input.txt'):
     return
 
 def process():
-    process_a()
-    process_b()
+    print(process_a('input.txt'))
+    print(process_b('input-1.txt'))
 
 if __name__ == "__main__":
     process()
