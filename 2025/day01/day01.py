efdb24e67f6aa3ae9b7519fc0f9db40b2ad578d3
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2025-12-01 10:12:46 +0000

    Started Day 1 challenge

---
+++ 2025/day01/day01.py
@@ -0,0 +1,13 @@
+#!env python3
+#
+# day01.py
+#
+def part1(dfile):
+    start = 50
+    combo = 0
+    with open(dfile, rt) as inf:
+        for i in inf:
+            print(i)
+    return(combo)
+
+print(part1('test.txt'))
