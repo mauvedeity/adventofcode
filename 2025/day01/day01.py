a57f0f183610aed9bc9f8669e2c1b8ddd0470c83
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2025-12-01 10:26:20 +0000

    Completed Day 1 Part 1

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -2,12 +2,33 @@
 #
 # day01.py
 #
+# Part 1
 def part1(dfile):
     start = 50
     combo = 0
-    with open(dfile, rt) as inf:
+    with open(dfile, 'rt') as inf:
         for i in inf:
-            print(i)
+            if(len(i) > 1):
+                dir = i[0]  
+                stp = int(i[1:])
+            if(dir == 'L'):
+                start = (start - stp) % 100
+            else:
+                start = (start + stp) % 100
+            if(start == 0):
+                combo += 1
     return(combo)
 
-print(part1('test.txt'))
+# Part 2
+def part2(dfile):
+    start = 50
+    combo = 0
+    with open(dfile, 'rt') as inf:
+        for i in inf:
+            if(len(i) > 1):
+                dir = i[0]
+                stp = int(i[1:])
+            
+    return(combo)
+
+print('Combo:', part1('input-a.txt'))
