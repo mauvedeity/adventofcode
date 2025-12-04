e363213b5d08732b309e5f112fdeae21636687da
Rhys Wilkins <rhys.wilkins@sainsburys.co.uk>
2025-12-04 11:13:45 +0000

    Tapping out here for the time being

---
+++ 2025/day04/day04.py
@@ -0,0 +1,14 @@
+#!env python3
+
+def process_a(inputfile):
+    return(0)
+
+def process_b(inputfile):
+    return(0)
+
+def process():
+    print(process_a('input.txt'))
+    print(process_b('input.txt'))
+
+if __name__ == "__main__":
+    process()
